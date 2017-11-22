968b96c85667c61229f210b2682c54da3bbb4c59
thunder011 <30837445+thunder011@users.noreply.github.com>
2017-11-22 10:46:08 -0800

    Se añadio un metodo para cambiar el color

--- webapp/admin/js/index.js
+++ webapp/admin/js/index.js
@@ -1,6 +1,23 @@
+var modoNocturno = false;
 function logout(){
 	window.location = '../index.html';
 }
 function confirmar() {
 	alert('Confirmar Venta?');
+}
+
+function modoN()
+{
+	if(!modoNocturno)
+	{	
+		modoNocturno = true;
+		document.getElementById('us').style.backgroundColor = '#BB0000';
+		document.getElementById('sidemenu').style.backgroundColor = '#BB0000';
+	}
+	else
+	{	
+		modoNocturno = false;
+		document.getElementById('us').style.backgroundColor = '#424242';
+		document.getElementById('sidemenu').style.backgroundColor = '#424242'
+	}
 }
