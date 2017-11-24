cc35cd5319207fa8ba827e962a39dd956f1b26d4
thunder011 <30837445+thunder011@users.noreply.github.com>
2017-11-24 11:47:37 -0800

    Se ha modificado para que todo sea en una pantalla

--- webapp/admin/js/index.js
+++ webapp/admin/js/index.js
@@ -1,23 +1,39 @@
-var modoNocturno = false;
+var urlHtml = 'http://localhost:8080/SeatOrganizer/admin/';
+
 function logout(){
 	window.location = '../index.html';
 }
 function confirmar() {
 	alert('Confirmar Venta?');
 }
-
-function modoN()
+function nuevoEvento()
 {
-	if(!modoNocturno)
-	{	
-		modoNocturno = true;
-		document.getElementById('us').style.backgroundColor = '#BB0000';
-		document.getElementById('sidemenu').style.backgroundColor = '#BB0000';
-	}
-	else
-	{	
-		modoNocturno = false;
-		document.getElementById('us').style.backgroundColor = '#424242';
-		document.getElementById('sidemenu').style.backgroundColor = '#424242'
-	}
+	console.log(urlHtml + 'nuevoEvento.html');
+	document.getElementById('charger').src = urlHtml + 'nuevoEvento.html';
+}
+function evento()
+{
+	document.getElementById('charger').src = urlHtml + 'modificarEvento.html';
+}
+function realizarVenta()
+{
+	console.log(urlHtml + 'realizarVenta.html');
+	document.getElementById('charger').src = urlHtml + 'realizarVenta.html';
+}
+function modificarVenta()
+{
+	document.getElementById('charger').src = urlHtml + 'modificarVenta.html';
+}
+function nuevoUsuario()
+{
+	console.log(urlHtml + 'agregarUsuario.html');
+	document.getElementById('charger').src = urlHtml + 'agregarUsuario.html';
+}
+function modificarUsuario()
+{
+	document.getElementById('charger').src = urlHtml + 'modificarUsuario.html';
+}
+function alerta()
+{	
+	alert("Desea Recargar?");
 }
