f67c45848097dd83786c9da65810bf9456ecde4f
Eric Juarez <eric.jrz@gmail.com>
2017-12-02 10:40:58 -0800

    Se anade funcionalidad visual. Permite seleccionar las butacas a vender, y deselecionarlas.

--- webapp/js/seatchart.js
+++ webapp/js/seatchart.js
@@ -4,6 +4,10 @@ function init() {
   // evento, seccion
   // Le agrega el ID a cada butaca dentro de la seccion seleccionada
   getSeatsId(1, 9);
+
+  // add onClick to does seats availible to sell
+  // allows to select seats to sell
+  setTimeout(setEventSeatsSell, 3000);
 }
 
 function getSeatsId(eventId, sectionId) {
@@ -29,7 +33,7 @@ function getSeatsId(eventId, sectionId) {
 // sets id's to seats
 function setSeatId(eventId, data) {
   var JSONdata = JSON.parse(data);
-  var seats = document.getElementsByClassName("seat");
+  var seats = document.getElementsByClassName("available");
   var seat = 0;
 
   // sets all the id's to each seat from the section
@@ -41,31 +45,26 @@ function setSeatId(eventId, data) {
   }
 
   // gets all seats bougth from the section
-  for (var i = 0; i < seats.length; i++) {
+    for (var i = 0; i < seats.length; i++) {
     getSeatsBougth(eventId, seats[i].id);
   }
 }
 
 function getSeatsBougth(eventId, seatId) {
-  // Create request
   var x = new XMLHttpRequest();
-  // Prepare request
   x.open('GET', 'http://localhost/stadium/webapp/apis/boleto.php?evento=' + eventId + '&butaca=' + seatId, true);
-  // Send request
   x.send();
 
-  // Handle readyState change event
   x.onreadystatechange = function() {
-    // check status
-    //status : 200=OK, 404=Page not found, 500=server denied access
-    // readyState : 4=Back with data
     if (x.status == 200 && x.readyState == 4) {
-      //show buildings
       try {
+        // sets different color to each seat bougth
         viewSeatsBougth(x.responseText);
       } catch(e) { }
     }
   }
+
+
 }
 
 // sets different color to each seat bougth
@@ -73,7 +72,30 @@ function viewSeatsBougth(data) {
   var JSONdata = JSON.parse(data);
   var seatId = JSONdata.butaca;
 
-  // change fill color
+  // change fill color, taked from class 'selled'
   var seat = document.getElementById(seatId);
-  seat.style.fill = "lightgray";
+  seat.setAttribute("class", "selled");
+}
+
+function setEventSeatsSell() {
+  var seats = document.getElementsByClassName("available");
+  var i = 0;
+  for ( i = 0; i < seats.length; i++) {
+    var seat = document.getElementById(seats[i].id);
+    seat.onclick = function() { toBuy(this) };
+    //seat.addEventListener('click', function() { toBuy(this.id); }); -- The addEventListener() method is not supported in Internet Explorer 8 and earlier versions.
+    console.log(seat.id);
+  }
+
+}
+
+function toBuy(seat) {
+  seat.setAttribute("class", "selected");
+  seat.onclick = function() { setAvailableAgain(this) };
+  // add to list to sell all boletos
+}
+
+function setAvailableAgain(seat) {
+  seat.setAttribute("class", "available");
+  seat.onclick = function() { toBuy(this) };
 }
